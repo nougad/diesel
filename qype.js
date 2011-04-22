0cd460721a51ce3ff49676da857f02411ee31416
Florian Eitel <feitel@indeedgeek.de>
2011-04-23 00:57:57 +0200

    changed to getCurrentGeoLocation method

--- qype.js
+++ qype.js
@@ -1,3 +1,4 @@
+// TODO wth is var?
 function getCategories(callback, parent) {
   if (parent == undefined) {
     return $.getJSON("http://api.qype.com/v1/place_categories.json?consumer_key="+apikey+"&callback=?", callback);
@@ -9,4 +10,22 @@ function getCategories(callback, parent) {
   }
 }
 
+function getCurrentGeoLocation(callback) {
+  var errorCallback = function(error) {
+    // TODO something usefull
+    $('<h2>Oops: GPS fail ' + error.code+'</h2>').appendTo('body');
+  }
 
+  var successCallback = function(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    callback(latitude, longitude);
+  }
+
+  if (typeof (navigator.geolocation) != 'undefined') {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback, { enableHighAccuracy: true, maximumAge: 10000 });
+  } else {
+    // TODO something usefull
+    $('<h2>Oops: No location available</h2>').appendTo('body');
+  }
+}
